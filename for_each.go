f2a657f648845bbffee2603bc6c5e52c4e03fcc7
AnthonyLons <anthonyguera551@gmail.com>
2026-05-26 12:34:42 -0400

    Add for each implementation

--- for_each.go
+++ for_each.go
@@ -1,6 +1,19 @@
 package radixtree
 
-// ForEach calls fn for each word in the tree. If fn returns false, iteration stops.
-func (r *RadixTree) ForEach(_ func(key string) bool) {
-	panic("unimplemented")
+// ForEach calls fn for each word in the tree.
+func (r *RadixTree) ForEach(fn func(key string)) {
+	r.forEach(r.root, "", fn)
+}
+
+func (r *RadixTree) forEach(cursor *node, currentWord string, fn func(string)) {
+	if cursor == nil {
+		return
+	}
+	if cursor.isTerminal {
+		fn(currentWord + cursor.prefix)
+	}
+	currentWord += cursor.prefix
+	for _, child := range cursor.children {
+		r.forEach(child, currentWord, fn)
+	}
 }
