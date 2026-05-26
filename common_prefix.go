50cb54d45542a717eadfa5fe268dc779dc79c4e1
AnthonyLons <anthonyguera551@gmail.com>
2026-05-26 12:47:32 -0400

    Working base API

--- common_prefix.go
+++ common_prefix.go
@@ -1,6 +1,22 @@
 package radixtree
 
+import "strings"
+
 // CommonPrefix returns the longest common prefix among all stored words.
 func (r *RadixTree) CommonPrefix() string {
-	panic("unimplemented")
+	if r.root == nil {
+		return ""
+	}
+	prefix := strings.Builder{}
+	cursor := r.root
+	for {
+		prefix.WriteString(cursor.prefix)
+		if cursor.isTerminal || len(cursor.children) > 1 {
+			break
+		}
+		for _, uniqueNode := range cursor.children {
+			cursor = uniqueNode
+		}
+	}
+	return prefix.String()
 }
--- string.go
+++
@@ -1,6 +0,0 @@
-package radixtree
-
-// String returns a string representation of the tree.
-func (r *RadixTree) String() string {
-	panic("unimplemented")
-}
